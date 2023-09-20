728fc9edf21ad7a9d44aea9776673fdb7066f78b
DmitroVolinskiy <kareivolik6@gmail.com>
2023-09-20 20:26:41 +0200

    task 2,3

---
+++ hw1/task2.py
@@ -0,0 +1,8 @@
+#lesson.nr.1:
+#-i like python
+#-i have 2 guitars
+print("lesson", "nr", "1:", sep=".", end="\n-")
+a="i like python"
+print(a, end="\n-")
+b=2
+print("i have", b, "guitars", sep=" ", end="\t")
---
+++ hw1/task3.py
@@ -0,0 +1,4 @@
+a="#########"
+b="#\t\t#"
+print(a, b, b, b, a, sep="\n")
+print(b, b, a, b, b, sep="\n")
