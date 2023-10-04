ceea719ea17bed5412aa70660b5c2834bcdebd01
DmitroVolinskiy <kareivolik6@gmail.com>
2023-10-04 23:06:58 +0200

    first try

---
+++ hw2/task1.py
@@ -0,0 +1,3 @@
+name="dmytro"
+day="today"
+print( "Good day ",name.capitalize(), "! ", day.capitalize(), " is a perfect day to learn some python.",sep ="")
---
+++ hw2/task2.py
@@ -0,0 +1,3 @@
+name = "Dmytro"
+last_name = "Volynskyi"
+print("Hello,", name, last_name, sep=" ")
